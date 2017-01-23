a222712880e90ca56e7bef30c8e7c7264e3921a5
Pablo Barton <pablo.barton@gmail.com>
2017-01-23 13:43:21 -0500

    some placeholder files

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+This is the setup file to install openaccess_epub.
+"""
+
+from distutils.core import setup
+#from setuptools import setup
+
+
+def long_description():
+    with open('README.md', 'r') as readme:
+        readme_text = readme.read()
+    return(readme_text)
+
+setup(name='tekstoj',
+      version='0.0.1',
+      description='Interface to text conversations via Twilio',
+      long_description=long_description(),
+      author='Paul Barton',
+      author_email='pablo.barton@gmail.com',
+      url='https://github.com/SavinaRoja/tekstoj',
+      package_dir={'': 'src'},
+      packages=['tekstoj'],
+#      package_data={},
+#      scripts=['scripts/'],
+      data_files=[('', ['README.md'])],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                   'Programming Language :: Python :: 3',
+                   'Operating System :: OS Independent'],
+      install_requires=['docopt', 'twilio', 'flask', 'sqlalchemy']
+      )
+
