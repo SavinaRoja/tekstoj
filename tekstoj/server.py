d9b431f8e8f9cb9635f6fdb7bca0e3353da299e6
Pablo Barton <pablo.barton@gmail.com>
2017-02-08 22:34:59 -0500

    starting model usage for message reception

--- tekstoj/server.py
+++ tekstoj/server.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, g
 from .models import Contact, Message
-from sqlalchemy import creat_engine
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 engine = create_engine('mysql+mysqldb://scott:tiger@localhost:5432/mydatabase')
 
-@app.route('/sms', methods=['POST'])
+@app.route('/receive', methods=['POST'])
 def sms():
     transmitted = dt.datetime.now()
     f = request.form
-    contact_number = f['From']
-    message_sid = f['MessageSid']
-    account_sid = f['AccountSid']
-    messaging_service_sid = f.get('MessagingServiceSid', '')
-    body = f['Body']
-    num_media = int(f['NumMedia'])
-    outbound = 0
-
-
+    msg = Message(sid=f['MessageSid'],
+                  datecreated=None,  # This is for outgoing
+                  dateupated=None,  # This is for outgoing
+                  datesent=None,  # This is for outgoing
+                  accountsid=f['AccountSid'],
+                  fromnumber=f['From'],
+                  tonumber=f['To'],
+                  body=f['body'],
+                  nummedia=int(f['NumMedia']),
+                  numsegments=int(f['NumSegments']),
+                  status=f['SmsStatus']
+                  errorcode=f.get('SmsErrorCode', None),  # Will we ever get one like this?
+                  errormessage=f.get('SmsErrorMessage', None),
+                  direction='inbound',  # safe to assume here
+                  price=f.get('Price', None),  # unconfirmed
+                  price=f.get('Price', None),  # unconfirmed
+                  apiversion=f['ApiVersion'],
+                  uri=f.get('Uri', None),  # unconfirmed
+                  subresourceuri=f.get('SubresourceUri', None),  #unconfirmed
+                  )
