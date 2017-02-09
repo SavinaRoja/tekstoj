f52d1f28658da12b1507498a5a90a8debe9da712
Pablo Barton <pablo.barton@gmail.com>
2017-02-08 21:58:56 -0500

    mysql is now happy with the table spec

--- tekstoj/models.py
+++ tekstoj/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -19,29 +19,30 @@ class Contact(Base):
 
 #Reference
 #https://www.twilio.com/docs/api/rest/message
+#String(32) seems safe for RFC 2882 strings, which I think are 31 characters
 class Message(Base):
     __tablename__ = 'messages'
 
-    sid          = Column(String, primary_key=True)
-    datecreated  = Column(String)
-    dateupdated  = Column(String)
-    datesent     = Column(String)
-    accountsid   = Column(String)
+    sid          = Column(String(34), primary_key=True)
+    datecreated  = Column(String(32))
+    dateupdated  = Column(String(32))
+    datesent     = Column(String(32))
+    accountsid   = Column(String(34))
     #messagingservicesid
-    fromnumber   = Column(String)
-    tonumber     = Column(String)
-    body         = Column(String)
+    fromnumber   = Column(String(11))
+    tonumber     = Column(String(11))
+    body         = Column(Text)
     nummedia     = Column(Integer)
     numsegments  = Column(Integer)
-    status       = Column(String)
-    errorcode    = Column(String)
-    errormessage = Column(String)
-    direction    = Column(String)
-    price        = Column(String)
-    princeunit   = Column(String)
-    apiversion   = Column(String)
-    uri          = Column(String)
-    subresourceuri = Column(String)
+    status       = Column(String(11))  # 'undelivered' is longest value
+    errorcode    = Column(String(5))
+    errormessage = Column(Text)  # The error mapping may as well be its own table
+    direction    = Column(String(14))  # 'outbound-reply' is longest value
+    price        = Column(String(16))  # safe limit, who knows
+    princeunit   = Column(String(3))
+    apiversion   = Column(String(12))  # like '2010-04-01', with extra 2
+    uri          = Column(Text)
+    subresourceuri = Column(Text)
 
     def __repr__(self):
         return '<Message(sid={}, fromnumber={}, tonumber={}, body={})>'.format(self.sid, self.fromnumber, self.tonumber, self.body)
